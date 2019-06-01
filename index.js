020e8a45a9a3de45655277c3589f23bd10f2b663
Zain Arif <zainarif14197@gmail.com>
2019-06-01 19:17:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -100,21 +100,21 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-app.use((req,res,next)=> {
-  const error = new Error('Not Found');
-  error.status = 404;
-  next(error)
-})
+// app.use((req,res,next)=> {
+//   const error = new Error('Not Found');
+//   error.status = 404;
+//   next(error)
+// })
 
 
-app.use((error,req,res,next)=> {
-    res.status(error.status || 7000);
-    res.json({
-        error: {
-        message: error.message
-        }
-    })
-})
+// app.use((error,req,res,next)=> {
+//     res.status(error.status || 7000);
+//     res.json({
+//         error: {
+//         message: error.message
+//         }
+//     })
+// })
 
 
 module.exports = app;
